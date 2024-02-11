caa9d583891b3c6ac74df07d138550f1577adddc
revaga <58123400+revaga@users.noreply.github.com>
2024-02-10 18:25:19 -0800

    thomas's help

---
+++ script.js
@@ -0,0 +1,14 @@
+let projects = [
+    {
+        "name": "Reva's Pretzel Factory"
+    },
+    {
+        "name": "We can build it"
+    }
+]
+
+console.log(projects)
+
+let trunk = document.getElementById("trunk")
+
+trunk.style.height = projects.length * 16 
