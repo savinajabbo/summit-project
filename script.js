d9964686485ca7f5a77829bb0d10a931012e51ef
revaga <58123400+revaga@users.noreply.github.com>
2024-02-11 00:12:48 -0800

    branch code update 1
    doesn't work yet, but the goals is to show a branch for every entry in the array

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
 let projects = [
     {
-        "name": "Reva's Pretzel Factory"
+        "name": "Reva's Pretzel Factory",
+        "hclubber": "Reva",
+        "description": "",
+        "url": ""
+
     },
     {
-        "name": "We can build it"
+        "name": "We can build it",
+        "hclubber" : "Bri",
+        "description": "",
+        "url": ""
+    },
+    {
+        "name": "Pickles",
+        "hclubber" : "Bri",
+        "description": "",
+        "url": ""
     }
+
 ]
 
 console.log(projects)
 
-let trunk = document.getElementById("trunk")
+//let branch = document.getElementById("branch")
 
-trunk.style.height = projects.length * 16 
+
+//branch.style.width = projects.length * 16
+
+
+
+for(let i = 0; i < projects.length; i++){
+        var temp = document.createElement('branch');
+        temp.innerText = projects[i]["name"];
+        console.log(temp);
+        temp.style.width = projects[i]["name"].length*20;
+        temp.style.backgroundColor = "#7a522f";
+        temp.style.height = 16;
+
+        //temp.classList.add("branch");
+    }
